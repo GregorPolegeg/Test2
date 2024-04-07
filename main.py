179fe419ec60247ac0ebb05cb54a8e2730dea56b
gregor <gregor.poleg3g@gmail.com>
2024-04-07 07:14:07 -0400

    Added main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import math
+
+def calculate_sqrt_of_9():
+    sqrt_9 = math.sqrt(9)
+    return sqrt_9
+
+def calculate_cube_of_3():
+    cube_3 = 3 ** 3
+    return cube_3
+
+if __name__ == "__main__":
+    sqrt_result = calculate_sqrt_of_9()
+    cube_result = calculate_cube_of_3()
+
+    print("Square root of 9:", sqrt_result)
+    print("Cube of 3:", cube_result)
