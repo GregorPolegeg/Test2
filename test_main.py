3ca71fc043d4603f5a9357c06a2bbb5bbb2db7ec
gregor <gregor.poleg3g@gmail.com>
2024-04-07 08:14:59 -0400

    added text_main.py

---
+++ test_main.py
@@ -0,0 +1,15 @@
+from main import calculate_sqrt, calculate_cube
+
+
+number_to_sqrt = 9
+expected_sqrt_result = 3
+sqrt_result = calculate_sqrt(number_to_sqrt)
+assert sqrt_result == expected_sqrt_result, f"Square root of {number_to_sqrt} should be {expected_sqrt_result}, but got {sqrt_result}"
+
+
+number_to_cube = 3
+expected_cube_result = 27
+cube_result = calculate_cube(number_to_cube)
+assert cube_result == expected_cube_result, f"Cube of {number_to_cube} should be {expected_cube_result}, but got {cube_result}"
+
+print("All tests passed successfully.")
