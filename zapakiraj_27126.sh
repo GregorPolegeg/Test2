0f210314ee9add842127195fb112a2e2ef2e7299
GregorPolegeg <79710446+GregorPolegeg@users.noreply.github.com>
2024-04-14 14:29:30 +0200

    Update zapakiraj_27126.sh

--- zapakiraj_27126.sh
+++ zapakiraj_27126.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "$DOCKER_ACCESS_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
 docker build -t $DOCKER_USERNAME/sysa:$GITHUB_SHA .
 docker push $DOCKER_USERNAME/sysa:$GITHUB_SHA
 
