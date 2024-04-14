dd0342b74426a43cec2058fb4fc1e063b30f6652
GregorPolegeg <79710446+GregorPolegeg@users.noreply.github.com>
2024-04-14 14:40:00 +0200

    Update zapakiraj_27126.sh

--- zapakiraj_27126.sh
+++ zapakiraj_27126.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-echo "$DOCKER_ACCESS_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t $DOCKER_USERNAME/sysa:$GITHUB_SHA . --password-stdin
-docker push $DOCKER_USERNAME/sysa:$GITHUB_SHA --password-stdin
-docker build -t $DOCKER_USERNAME/sysa:latest . --password-stdin
-docker push $DOCKER_USERNAME/sysa:latest --password-stdin
+echo "${DOCKER_ACCESS_TOKEN}" | docker login --username "${DOCKER_USERNAME}" --password-stdin
+
+# Zgradite sliko s SHA vrednostjo trenutnega commit-a
+docker build -t "${DOCKER_USERNAME}/sysa:${GITHUB_SHA}" .
+# Potisnite sliko s SHA vrednostjo na DockerHub
+docker push "${DOCKER_USERNAME}/sysa:${GITHUB_SHA}"
+
+# Zgradite sliko s oznako "latest"
+docker build -t "${DOCKER_USERNAME}/sysa:latest" .
+# Potisnite sliko s oznako "latest" na DockerHub
+docker push "${DOCKER_USERNAME}/sysa:latest"
