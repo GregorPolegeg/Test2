916e1ef57b6f746ce5cba014796273e3b85374e7
GregorPolegeg <79710446+GregorPolegeg@users.noreply.github.com>
2024-04-14 15:23:24 +0200

    Update zazeni_27126.sh

--- zazeni_27126.sh
+++ zazeni_27126.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-docker pull $DOCKER_USERNAME/sysa:$GITHUB_SHA
-docker run -d $DOCKER_USERNAME/sysa:$GITHUB_SHA
+
+# Define variables
+DOCKER_USERNAME="gregorpolegeg"
+REPO_NAME="sysa"
+TAG="latest"
+
+# Pull the latest image from Docker Hub
+docker pull $DOCKER_USERNAME/$REPO_NAME:$TAG
+
+# Run the container from the pulled image
+docker run -d --name sysa_container $DOCKER_USERNAME/$REPO_NAME:$TAG
+
+# Print a completion message
+echo "Container sysa_container is running using image $DOCKER_USERNAME/$REPO_NAME:$TAG"
