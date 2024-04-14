1600073792215f5d9cc977aaf78f7239111ec000
GregorPolegeg <79710446+GregorPolegeg@users.noreply.github.com>
2024-04-14 14:13:29 +0200

    Delete .github/workflows/zapakiraj_27126.sh

--- .github/workflows/zapakiraj_27126.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t $DOCKER_USERNAME/sysa:$GITHUB_SHA .
-docker push $DOCKER_USERNAME/sysa:$GITHUB_SHA
