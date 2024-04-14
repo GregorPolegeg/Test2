3e868ec4c95d9258e4e5543e835f8ec1d408810c
GregorPolegeg <79710446+GregorPolegeg@users.noreply.github.com>
2024-04-14 13:56:03 +0200

    Update zapakiraj_27126.sh

--- .github/workflows/zapakiraj_27126.sh
+++ .github/workflows/zapakiraj_27126.sh
@@ -1,20 +1,4 @@
-name: Deploy
-
-on:
-  workflow_run:
-    workflows: ["Test"]
-    branches: [main]
-    types:
-      - completed
-
-jobs:
-  deploy:
-    runs-on: self-hosted
-    if: github.event.workflow_run.conclusion == 'success'
-
-    steps:
-    - name: Checkout code
-      uses: actions/checkout@v3
-
-    - name: Build and Push Docker Image
-      run: ./zapakiraj_27126.sh
+#!/bin/bash
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker build -t $DOCKER_USERNAME/sysa:$GITHUB_SHA .
+docker push $DOCKER_USERNAME/sysa:$GITHUB_SHA
